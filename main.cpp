723f11c11d57920e854f71596b65ca9c98b13c59
htkhiem <42716602+htkhiem@users.noreply.github.com>
2020-02-25 22:33:03 +0700

    Refactor boundary drawing code

--- main.cpp
+++ main.cpp
@@ -10,8 +10,23 @@
 
 #define WIDTH 80
 #define HEIGHT 24
-#define ITR 500
+#define ITR 250
 using namespace std; // test
+
+void DrawBorder(WINDOW *main_window, size_t width, size_t height, int score) {
+  // Draw border
+  wborder(main_window, ACS_VLINE, ACS_VLINE, ACS_HLINE, ACS_HLINE, ACS_ULCORNER,
+          ACS_URCORNER, ACS_LLCORNER, ACS_LRCORNER);
+
+  // Draw title bar
+  mvwprintw(main_window, 1, 1, "Score: ");
+  wprintw(main_window, "%d", score);
+
+  // Draw horizontal separator
+  mvwaddch(main_window, 2, 0, ACS_LTEE);
+  whline(main_window, ACS_HLINE, width - 1);
+  mvwaddch(main_window, 2, width - 1, ACS_RTEE);
+}
 
 int main(int argc, char **argv) {
   initscr();       // Init ncurses window
@@ -28,96 +43,85 @@ int main(int argc, char **argv) {
   int score = 0; // Starts at snake length = 2
 
   srand(time(nullptr));
-  int food_x = rand() % WIDTH, food_y = rand() % HEIGHT;
+  int food_x = rand() % (WIDTH - 3) + 1, food_y = rand() % (HEIGHT - 3) + 1;
 
   try {
+    char head_shape = ACS_RARROW;
     while (input != 'q' && input != 'Q') {
       // Check if snake's eaten the food piece
       if (s.front->x == food_x && s.front->y == food_y) {
         s.grow();
         score++;
-        food_x = rand() % WIDTH;
-        food_y = rand() % HEIGHT;
+        food_x = rand() % (WIDTH - 3) + 1;
+        food_y = rand() % (HEIGHT - 3) + 1;
       } else {
         wclear(main_window);
-        SnakeSegment *cursor = s.back;
-
-        // Draw snake
-        while (cursor->next) {
-          mvwaddch(main_window, cursor->y + 2, cursor->x, '*');
-          cursor = cursor->next;
-        }
-        // Snake head
-        switch (s.current_dir) {
-        case (Direction::up):
-          mvwaddch(main_window, cursor->y + 2, cursor->x, '^');
-          break;
-        case (Direction::left):
-          mvwaddch(main_window, cursor->y + 2, cursor->x, '<');
-          break;
-        case (Direction::down):
-          mvwaddch(main_window, cursor->y + 2, cursor->x, 'V');
-          break;
-        case (Direction::right):
-          mvwaddch(main_window, cursor->y + 2, cursor->x, '>');
-          break;
-        }
-
-        // Draw food
-        mvwaddch(main_window, food_y + 2, food_x, 'o');
-
-        // Draw border and update window
-        wborder(main_window, ACS_VLINE, ACS_VLINE, ACS_HLINE, ACS_HLINE,
-                ACS_ULCORNER, ACS_URCORNER, ACS_LLCORNER, ACS_LRCORNER);
-
-        // Draw title bar
-        mvwprintw(main_window, 1, 1, "Score: ");
-        wprintw(main_window, "%d", score);
-        wmove(main_window, 2, 0);
-	waddch(main_window, ACS_LTEE);
-        whline(main_window, ACS_HLINE, WIDTH - 1);
-	wmove(main_window, 2, WIDTH - 1);
-	waddch(main_window, ACS_RTEE);
-        wrefresh(main_window);
-
-        usleep(1000 * ITR);
 
         // Get input and move snake
         input = wgetch(main_window);
         switch (input) {
-        case 'x':
-        case 'X':
-          s.grow();
-          score++;
-          break;
         case 'w':
         case 'W':
-          s.move(Direction::up);
+          if (s.current_dir != Direction::down) {
+            s.move(Direction::up);
+            head_shape = ACS_UARROW;
+          } else
+            s.move(Direction::down);
           break;
         case 'a':
         case 'A':
-          s.move(Direction::left);
+          if (s.current_dir != Direction::right) {
+            s.move(Direction::left);
+            head_shape = ACS_LARROW;
+          } else
+            s.move(Direction::right);
           break;
         case 's':
         case 'S':
-          s.move(Direction::down);
+          if (s.current_dir != Direction::up) {
+            s.move(Direction::down);
+            head_shape = ACS_DARROW;
+          } else
+            s.move(Direction::up);
           break;
         case 'd':
         case 'D':
-          s.move(Direction::right);
+          if (s.current_dir != Direction::left) {
+            s.move(Direction::right);
+            head_shape = ACS_RARROW;
+          } else
+            s.move(Direction::left);
           break;
         default:
           s.move(s.current_dir);
           break;
         }
+
+        SnakeSegment *cursor = s.back;
+        // Draw snake body
+        while (cursor->next) {
+          mvwaddch(main_window, cursor->y + 2, cursor->x, '*');
+          cursor = cursor->next;
+        }
+        // Head drawn last
+        mvwaddch(main_window, cursor->y + 2, cursor->x, head_shape);
+
+        // Draw food
+        mvwaddch(main_window, food_y + 2, food_x, ACS_DIAMOND);
+
+        // Draw UI
+        DrawBorder(main_window, WIDTH, HEIGHT, score);
+
+        // Refresh
+        wrefresh(main_window);
+        usleep(1000 * ITR);
       }
     }
   } catch (exception &e) {
     wclear(main_window);
     string game_over = "GAME OVER", why = e.what(),
            instr = "Press any key to exit...";
-    wborder(main_window, ACS_VLINE, ACS_VLINE, ACS_HLINE, ACS_HLINE,
-            ACS_ULCORNER, ACS_URCORNER, ACS_LLCORNER, ACS_LRCORNER);
+    DrawBorder(main_window, WIDTH, HEIGHT, score);
     mvwprintw(main_window, floor(HEIGHT / 2) - 2,
               floor((WIDTH - game_over.length()) / 2), game_over.c_str());
     mvwprintw(main_window, floor(HEIGHT / 2),
