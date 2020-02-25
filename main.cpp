e2d1d6fe1c8f33801aee124dd561af5242d247ee
htkhiem <42716602+htkhiem@users.noreply.github.com>
2020-02-25 19:25:18 +0700

    Game over screen

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 // Snake game, ncurses version
-#include "utils.h"
 #include "snake.hpp"
-#include <ncurses.h>
-#include <unistd.h>
+#include "utils.h"
 #include <cstdlib> // simple rand() will do
 #include <ctime>
+#include <ncurses.h>
+#include <string>
+#include <unistd.h>
+#include <cstring>
 
 #define WIDTH 80
 #define HEIGHT 24
@@ -12,68 +14,91 @@
 using namespace std; // test
 
 int main(int argc, char **argv) {
-	initscr(); // Init ncurses window
-	noecho(); // Don't display keypresses
-	curs_set(FALSE); // Don't display cursor
-	WINDOW *main_window = newwin(HEIGHT, WIDTH, (LINES-HEIGHT)/2, (COLS-WIDTH)/2);
-	nodelay(main_window, true); // Snake never stops
-	
-	char input = 0;
-	Snake s(WIDTH, HEIGHT);
-	
-	int food_x = rand() % WIDTH, food_y = rand() % HEIGHT;
-	srand(time(nullptr));
-	
-	while (input != 'q' && input != 'Q') {
-		// Check if snake's eaten the food piece
-		if (s.front->x == food_x && s.front->y == food_y) {
-			s.grow();
-			food_x = rand() % WIDTH;
-			food_y = rand() % HEIGHT;
-		}
-		
-		wclear(main_window);
-		SnakeSegment* cursor = s.back;
-		
-		// Draw snake
-		while (cursor) {
-			mvwaddch(main_window, cursor->y, cursor->x, '*');
-			cursor = cursor->next;
-		}
-		
-		// Draw food
-		mvwaddch(main_window, food_y, food_x, 'o');
-		
-		// Draw border and update window
-		wborder(main_window, '|', '|', '-', '-', '+', '+', '+', '+');
-		wrefresh(main_window);
-		
-		usleep(1000*ITR);
-		
-		// Get input and move snake
-		input = wgetch(main_window);
-		switch (input) {
-			case 'x': case 'X':
-				s.grow();		
-				break;
-			case 'w': case 'W':
-				s.move(Direction::up);
-				break;
-			case 'a': case 'A':
-				s.move(Direction::left);
-				break;
-			case 's': case 'S':
-				s.move(Direction::down);
-				break;
-			case 'd': case 'D':
-				s.move(Direction::right);
-				break;
-			default:
-			 	s.move(s.current_dir);
-			 	break;
-		}
-	}
-	
-	endwin();
-	return 0;
+  initscr();       // Init ncurses window
+  noecho();        // Don't display keypresses
+  curs_set(FALSE); // Don't display cursor
+  WINDOW *main_window =
+      newwin(HEIGHT, WIDTH, (LINES - HEIGHT) / 2, (COLS - WIDTH) / 2);
+  nodelay(main_window, true); // Snake never stopsf
+
+  char input = 0;
+  Snake s(WIDTH, HEIGHT);
+
+  int food_x = rand() % WIDTH, food_y = rand() % HEIGHT;
+  srand(time(nullptr));
+  try {
+    while (input != 'q' && input != 'Q') {
+      // Check if snake's eaten the food piece
+      if (s.front->x == food_x && s.front->y == food_y) {
+        s.grow();
+        food_x = rand() % WIDTH;
+        food_y = rand() % HEIGHT;
+      }
+
+      wclear(main_window);
+      SnakeSegment *cursor = s.back;
+
+      // Draw snake
+      while (cursor) {
+        mvwaddch(main_window, cursor->y, cursor->x, '*');
+        cursor = cursor->next;
+      }
+
+      // Draw food
+      mvwaddch(main_window, food_y, food_x, 'o');
+
+      // Draw border and update window
+      wborder(main_window, '|', '|', '-', '-', '+', '+', '+', '+');
+      wrefresh(main_window);
+
+      usleep(1000 * ITR);
+
+      // Get input and move snake
+      input = wgetch(main_window);
+      switch (input) {
+      case 'x':
+      case 'X':
+        s.grow();
+        break;
+      case 'w':
+      case 'W':
+        s.move(Direction::up);
+        break;
+      case 'a':
+      case 'A':
+        s.move(Direction::left);
+        break;
+      case 's':
+      case 'S':
+        s.move(Direction::down);
+        break;
+      case 'd':
+      case 'D':
+        s.move(Direction::right);
+        break;
+      default:
+        s.move(s.current_dir);
+        break;
+      }
+    }
+  } catch (exception &e) {
+    wclear(main_window);
+    string game_over = "GAME OVER", why = e.what(),
+           instr = "Press any key to exit...";
+    wborder(main_window, '|', '|', '-', '-', '+', '+', '+', '+');
+    mvwprintw(main_window, floor(HEIGHT / 2) - 2,
+              floor((WIDTH - game_over.length()) / 2), game_over.c_str());
+    mvwprintw(main_window, floor(HEIGHT / 2),
+              floor((WIDTH - strlen(e.what())) / 2), e.what());
+    mvwprintw(main_window, floor(HEIGHT / 2) + 2,
+              floor((WIDTH - instr.length()) / 2), instr.c_str());
+    wrefresh(main_window);
+    char wait;
+    cin >> wait;
+    endwin();
+    return 0;
+  }
+  wgetch(main_window);
+  endwin();
+  return 0;
 }
--- snake.cpp
+++ snake.cpp
@@ -1,67 +1,65 @@
 #include "snake.hpp"
 
-SnakeSegment::SnakeSegment(int _x, int _y): x(_x), y(_y), next(nullptr) {}
+SnakeSegment::SnakeSegment(int _x, int _y) : x(_x), y(_y), next(nullptr) {}
 
-Snake::Snake(int width, int height) : 
-	current_dir(Direction::right), growing(false), w(width), h(height) {
-	back = new SnakeSegment(ceil(w/2), ceil(h/2));
-	front = new SnakeSegment(ceil(w/2) + 1, ceil(h/2));
-	back->next = front;
-	
+Snake::Snake(int width, int height)
+    : current_dir(Direction::right), growing(false), w(width), h(height) {
+  back = new SnakeSegment(ceil(w / 2), ceil(h / 2));
+  front = new SnakeSegment(ceil(w / 2) + 1, ceil(h / 2));
+  back->next = front;
 }
 
 Snake::~Snake() { // Start from back of snake (front of queue)
-	SnakeSegment* temp = back, * temp_next = back->next;
-	do {
-		delete temp;
-		temp = temp_next;
-		temp_next = temp_next->next;
-	} while (temp_next);
+  SnakeSegment *temp = back, *temp_next = back->next;
+  do {
+    delete temp;
+    temp = temp_next;
+    temp_next = temp_next->next;
+  } while (temp_next);
 }
 
-void Snake::grow() {
-	growing = true;
-}
+void Snake::grow() { growing = true; }
 
-void Snake::move(Direction move_dir) { // TODO: collison detection
-	current_dir = move_dir;
-	SnakeSegment* new_segment = new SnakeSegment(front->x, front->y);
-	switch (current_dir) {
-		case Direction::left:
-			new_segment->x--;
-			break;
-		case Direction::up:
-			new_segment->y--;
-			break;
-		case Direction::right:
-			new_segment->x++;
-			break;
-		case Direction::down:
-			new_segment->y++;
-			break;
-	}
-	
-	// Wall collison detection
-	if (new_segment->x < 0 || new_segment->x >= w ||
-		new_segment->y < 0 || new_segment->y >= h)
-		throw std::range_error("Snake hit wall");
+void Snake::move(Direction move_dir) {
+  current_dir = move_dir;
+  SnakeSegment *new_segment = new SnakeSegment(front->x, front->y);
+  switch (current_dir) {
+  case Direction::left:
+    new_segment->x--;
+    break;
+  case Direction::up:
+    new_segment->y--;
+    break;
+  case Direction::right:
+    new_segment->x++;
+    break;
+  case Direction::down:
+    new_segment->y++;
+    break;
+  }
 
-	// Bite detection
-	else {
-		SnakeSegment* cursor = back;
-		while (cursor) {
-			if (cursor->x == new_segment->x && cursor->y == new_segment->y)
-				throw range_error("Snake bit itself");
-			cursor = cursor->next;
-		}
-	}
-	
-	front->next = new_segment;
-	front = new_segment;
-	if (growing) growing = false;
-	else {
-		SnakeSegment* new_back = back->next;
-		delete back;
-		back = new_back;
-	}
+  // Wall collison detection
+  if (new_segment->x < 0 || new_segment->x >= w || new_segment->y < 0 ||
+      new_segment->y >= h)
+    throw std::range_error("Snake hit wall!");
+
+  // Bite detection
+  else {
+    SnakeSegment *cursor = back;
+    while (cursor) {
+      if (cursor->x == new_segment->x && cursor->y == new_segment->y)
+        throw range_error("Snake bit itself!");
+      cursor = cursor->next;
+    }
+  }
+
+  front->next = new_segment;
+  front = new_segment;
+  if (growing)
+    growing = false;
+  else {
+    SnakeSegment *new_back = back->next;
+    delete back;
+    back = new_back;
+  }
 }
